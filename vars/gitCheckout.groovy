823e6ca7f054a7a80245c7991ff023881095d0a3
navinprasadk <navinprasad0808@gmail.com>
2020-02-19 20:21:45 +0530

    added groovy script for checkout

---
+++ vars/gitCheckout.groovy
@@ -0,0 +1,8 @@
+checkout([
+    $class: 'GitSCM', 
+    branches: [[name: '*/master']], 
+    doGenerateSubmoduleConfigurations: false, 
+    extensions: [], 
+    submoduleCfg: [], 
+    userRemoteConfigs: [[url: 'https://github.com/navinprasadk/Shared-Library.git']]
+    ])
